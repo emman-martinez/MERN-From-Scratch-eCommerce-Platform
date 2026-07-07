1c293fe15d0037155dd182f1e41cacd07eba0700
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-07-06 22:02:18 -0600

    feat: 20. Modeling Our Data

--- backend/src/data/mongo/models/product.model.ts
+++ backend/src/data/mongo/models/product.model.ts
@@ -1,5 +1,34 @@
 import { model, Schema } from 'mongoose';
 
-const productSchema = new Schema({});
+const reviewSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const productSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    reviews: [reviewSchema],
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const ProductModel = model('Product', productSchema);
---
+++ backend/src/data/mongo/models/user.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = model('User', userSchema);
