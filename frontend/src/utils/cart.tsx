31b1991eb6fed16a653e7291a1f38c92c6f1a78c
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-07-10 11:44:40 -0600

    feat: 35. Cart Utils File

---
+++ frontend/src/utils/cart.tsx
@@ -0,0 +1,40 @@
+export const addDecimals = (num: number) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+interface CartState {
+  cartItems: { price: number; qty: number }[];
+  itemsPrice: string;
+  shippingPrice: string;
+  taxPrice: string;
+  totalPrice: string;
+}
+
+export const updateCart = (state: CartState) => {
+  // Calculate items price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce(
+      // acc is the accumulator, item is the current item in the array
+      // The reduce function takes a callback function and an initial value (0 in this case)
+      (acc: number, item: { price: number; qty: number }) => acc + item.price * item.qty,
+      0,
+    ),
+  );
+
+  // Calculate shipping price (if order is over $100 then free, else $10 shipping)
+  state.shippingPrice = addDecimals(Number(state.itemsPrice) > 100 ? 0 : 10);
+
+  // Calculate tax price (15% of items price)
+  state.taxPrice = addDecimals(Number((0.15 * Number(state.itemsPrice)).toFixed(2)));
+
+  // Calculate total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
+};
