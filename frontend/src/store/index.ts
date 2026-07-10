74dad2f4dfd5e0fb877247e0ddc428d2ab03ff7e
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-07-09 20:46:18 -0600

    feat: 32. Cart Slice & Reducer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./slices/productSlice";
+import { cartReducer, productReducer } from "./slices";
 
 export const store = configureStore({
   reducer: {
+    cart: cartReducer,
     product: productReducer,
   },
   devTools: true,
