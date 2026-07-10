dd92cc85ee82b9acbe36a33b359678c420c26415
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-07-10 10:32:26 -0600

    feat: 33. Add To Cart Function

--- frontend/src/store/slices/cartSlice.ts
+++ frontend/src/store/slices/cartSlice.ts
@@ -4,10 +4,55 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart") as string)
   : { cartItems: [] };
 
+const addDecimals = (num: number) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((c: { _id: string }) => c._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((c: { _id: string }) =>
+          c._id === existItem._id ? item : c,
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce(
+          // acc is the accumulator, item is the current item in the array
+          // The reduce function takes a callback function and an initial value (0 in this case)
+          (acc: number, item: { price: number; quantity: number }) =>
+            acc + item.price * item.quantity,
+          0,
+        ),
+      );
+
+      // Calculate shipping price (if order is over $100 then free, else $10 shipping)
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // Calculate tax price (15% of items price)
+      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+      // Calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
